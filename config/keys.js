c02ece32d4d1cbf4e2fb01ce94edd2c78f2c1241
Deepthi7796 <deepthi.psty@gmail.com>
2025-02-06 22:01:41 -0600

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://smart-health:<dbPassword>@smart-health.ztwhw.mongodb.net/Smart_Health?retryWrites=true&w=majority&appName=smart-health';
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
